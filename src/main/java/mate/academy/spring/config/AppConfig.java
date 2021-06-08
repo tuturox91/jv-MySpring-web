425a0a46e0043eb261bb9557cc42a95aa14186b1
resci <vlad.evdockimov.062@gmail.com>
2021-06-08 09:24:44 +0300

    Remove Application class, simplify package in ComponentScan

--- src/main/java/mate/academy/spring/config/AppConfig.java
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -14,8 +14,7 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 @Configuration
 @PropertySource("classpath:db.properties")
 @ComponentScan(basePackages = {
-        "mate.academy.spring.dao",
-        "mate.academy.spring.service"
+        "mate.academy.spring",
 })
 public class AppConfig {
     private final Environment env;
